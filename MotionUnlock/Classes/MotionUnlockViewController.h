9454bda01f9adc312e226b3b3a06e86c057172c4
Brian Reber <breber@iastate.edu>
2010-02-25 00:59:25 +0000

    -Changed the Header of the files. -Added some basic accelerometer stuff in (I'm not even sure if it works)
    git-svn-id: http://iphone-motion-unlock.googlecode.com/svn/trunk@5 a90dca44-21a1-11df-a7a9-25a45d9a8882

--- MotionUnlock/Classes/MotionUnlockViewController.h
+++ MotionUnlock/Classes/MotionUnlockViewController.h
@@ -2,15 +2,20 @@
 //  MotionUnlockViewController.h
 //  MotionUnlock
 //
-//  Created by Brian Reber on 2/24/10.
-//  Copyright Iowa State University 2010. All rights reserved.
+//  Created by Brian Reber, Todd Lyon, Ashley Nelson on 2/24/10.
+//  Copyright 2010. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface MotionUnlockViewController : UIViewController {
+@interface MotionUnlockViewController : UIViewController <UIAccelerometerDelegate> {
 
+	UIAccelerometer *accelerometer;
+	
 }
+
+@property (nonatomic, retain) UIAccelerometer *accelerometer;
+
 
 @end
 
--- MotionUnlock/Classes/MotionUnlockViewController.m
+++ MotionUnlock/Classes/MotionUnlockViewController.m
@@ -2,15 +2,15 @@
 //  MotionUnlockViewController.m
 //  MotionUnlock
 //
-//  Created by Brian Reber on 2/24/10.
-//  Copyright Iowa State University 2010. All rights reserved.
+//  Created by Brian Reber, Todd Lyon, Ashley Nelson on 2/24/10.
+//  Copyright 2010. All rights reserved.
 //
 
 #import "MotionUnlockViewController.h"
 
 @implementation MotionUnlockViewController
 
-
+@synthesize accelerometer;
 
 /*
 // The designated initializer. Override to perform setup that is required before the view is loaded.
@@ -29,12 +29,18 @@
 */
 
 
-/*
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
+	
+	self.accelerometer = [UIAccelerometer sharedAccelerometer];
+	self.accelerometer.updateInterval = .1;
+	self.accelerometer.delegate = self;
+	
+//	[[UIAccelerometer sharedAccelerometer] setUpdateInterval:1.0 / 60.0];
+//    [[UIAccelerometer sharedAccelerometer] setDelegate:self];
 }
-*/
+
 
 
 /*
@@ -44,6 +50,17 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 */
+
+
+/**
+ This is a method I found online.  We will need to figure out what to do to get it to work.
+ I think we will need to actually put the app on a device to test accelerometer data.
+ I will check into that.  I am pretty sure the department has devices that are registered...
+ */
+-(void)accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)acceleration
+{
+	NSLog(@"%f, %f, %f", acceleration.x,acceleration.y,acceleration.z);
+}
 
 - (void)didReceiveMemoryWarning {
 	// Releases the view if it doesn't have a superview.
