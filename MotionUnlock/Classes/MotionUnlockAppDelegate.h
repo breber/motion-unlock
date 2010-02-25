b1baf77c689e5f2612f0becf4baebd758c4f1785
Brian Reber <breber@iastate.edu>
2010-02-25 00:57:59 +0000

    -Changed the Header of the files. -Added some basic accelerometer stuff in (I'm not even sure if it works)
    git-svn-id: http://iphone-motion-unlock.googlecode.com/svn/trunk@4 a90dca44-21a1-11df-a7a9-25a45d9a8882

--- MotionUnlock/Classes/MotionUnlockAppDelegate.h
+++ MotionUnlock/Classes/MotionUnlockAppDelegate.h
@@ -2,8 +2,8 @@
 //  MotionUnlockAppDelegate.h
 //  MotionUnlock
 //
-//  Created by Brian Reber on 2/24/10.
-//  Copyright Iowa State University 2010. All rights reserved.
+//  Created by Brian Reber, Todd Lyon, Ashley Nelson on 2/24/10.
+//  Copyright 2010. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
