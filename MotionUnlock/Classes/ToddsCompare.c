0d1ee337e067047e089276c4a3e50cff99742cd2
Todd Lyon <toddlyon@iastate.edu>
2010-04-21 18:57:28 +0000

    This may work . . . dun dun duuuunnnn
    git-svn-id: http://iphone-motion-unlock.googlecode.com/svn/trunk@56 a90dca44-21a1-11df-a7a9-25a45d9a8882

--- MotionUnlock/Classes/ToddsCompare.c
+++ MotionUnlock/Classes/ToddsCompare.c
@@ -10,7 +10,8 @@
 #include "ToddsCompare.h"
 
 #define DATA_NUM 3000
-#define LENGTH 10
+//length is the length of the incoming data, passed in
+#define LENGTH 100
 #define SAMPLE_LENGTH 10
 #define TOLERANCE 0.1
 #define TRUE 1
@@ -18,6 +19,8 @@
 #define START 10
 
 int compareCaller(double[] x_data, double[] y_data, double[] z_data, double[] x_compare, double[] y_compare, double[] z_compare){
+	int i =0;
+	
 	double[DATA_NUM] data_x_slopes;
 	double[DATA_NUM] data_y_slopes;
 	double[DATA_NUM] data_z_slopes;
@@ -34,19 +37,27 @@ int compareCaller(double[] x_data, double[] y_data, double[] z_data, double[] x_
 	calculateSlopes(z_compare, compare_z_slopes, START, START + SAMPLE_LENGTH);
 	
 	//Find slopes for data to compare
-	calculateSlopes(<#int [] preValues#>, <#int [] postValues#>, <#int start#>, <#int end#>);
-	calculateSlopes(<#int [] preValues#>, <#int [] postValues#>, <#int start#>, <#int end#>);
-	calculateSlopes(<#int [] preValues#>, <#int [] postValues#>, <#int start#>, <#int end#>);
+	calculateSlopes(x_data, data_x_slopes, 0, LENGTH);
+	calculateSlopes(y_data, data_y_slopes, 0, LENGTH);
+	calculateSlopes(z_data, data_z_slopes, 0, LENGTH);
 	
+	int x_pass = 0, y_pass = 0, z_pass =0;
 	
+	//Compare Slopes
+	for (i = 0; i < LENGTH - SAMPLE_LENGTH; i++) {
+		x_pass = compare(compare_x_slopes, data_x_slopes, i, i + SAMPLE_LENGTH);
+		y_pass = compare(compare_y_slopes, data_y_slopes, i, i + SAMPLE_LENGTH);
+		z_pass = compare(compare_z_slopes, data_z_slopes, i, i + SAMPLE_LENGTH);
+		printf("%d, %d, %d \n", x_pass, y_pass, z_pass);
+	}
 	
 }
 
-void calculateSlopes(int preValues[], int postValues[], int start, int end){
+void calculateSlopes(double preValues[], double postValues[], int start, int end){
 	int i;
 	int j = 0;
 	for (i = start; i < end; i++) {
-		//add here
+		
 		double slope = (preValues[i+1]-preValues[i]);
 		
 		postValues[j] = slope;
@@ -57,9 +68,11 @@ int compare(double preSlope[], double postSlope[], int start, int end) {
 	
 	int difference = 0;
 	
-	for(int i = start; i <= end; i++) {
+	int i , j; 
+	
+	for(i = start; i <= end; i++) {
 		if(abs(postSlope[START] - preSlope[i]) <= TOLERANCE) {
-			for(int j = i + 1; j < SAMPLE_LENGTH - 1 && difference <= TOLERANCE; j++) {
+			for(j = i + 1; j < SAMPLE_LENGTH - 1 && difference <= TOLERANCE; j++) {
 				difference = abs(postSlope[j] - preSlope[j]);
 			}
 		}
