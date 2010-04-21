e4d9270c635bc9cec48e0f577c1c814c39ea2d80
Todd Lyon <toddlyon@iastate.edu>
2010-04-21 18:00:09 +0000

    Added a variation on the comparison so that we can make it more adaptable.
    git-svn-id: http://iphone-motion-unlock.googlecode.com/svn/trunk@50 a90dca44-21a1-11df-a7a9-25a45d9a8882

---
+++ MotionUnlock/Classes/ToddsCompare.h
@@ -0,0 +1,10 @@
+/*
+ *  ToddsCompare.h
+ *  MotionUnlock
+ *
+ *  Created by Todd Lyon on 4/21/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+int compare(double preSlope[], double postSlope[], int start, int end);
